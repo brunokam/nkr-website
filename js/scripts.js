d3935822ce579fb5391fbaf24a8240ec86e27202
Mateusz Bruno-Kaminski <m.bruno.kaminski@gmail.com>
2015-02-23 16:47:36 +0100

    Website update according to some suggestions
    Concerns #1

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,35 @@ $(document).on('ready', function() {
         }
     });
 
-    /*$('pre code').each(function(i, block) {
-        hljs.highlightBlock(block);
-    });*/
+    // Newsfeed fade in/out
+    $(window).scroll(function() {
+        var threshold = 30;
+        var coef = (threshold - $(window).scrollTop()) / threshold;
+        var top = (coef - 1) * threshold * 1.5;
+        $('.newsfeed, .under-constr').css({
+            'opacity': coef,
+            'top': (top <= 0 ? top + 51 : 51) + 'px'
+        });
+    });
+
+    // Parses RSS
+    $.getFeed({
+        url: '/news/feed/',
+        success: function (feed) {
+            var n = 0;
+            for (var i = 0; i < feed.items.length && n < 5; ++i) {
+                var item = feed.items[i];
+
+                if (item.title.length > 0) {
+                    $('<p class="item"><a href="'
+                    + item.link + '">'
+                    + item.title + '</a><span>'
+                    + item.updated.substr(0, item.updated.length - 9) + '</span></p>')
+                        .appendTo('.newsfeed .items');
+
+                    ++n;
+                }
+            }
+        }
+    });
 });
